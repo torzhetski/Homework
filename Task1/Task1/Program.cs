15ec978e4cf9f633e65dfa21269c5c7dc6d29257
Захар Торжецкий <torzhetski@mail.ru>
2022-08-07 15:01:37 +0300

    готово

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -5,11 +5,18 @@ class Program
     {
         string fullName = "Torzhertski Zakhar Vitalievich";
         string email = "torzhetski@mail.ru";
-        int age = 18;
-        double math = 10;
-        double physics = 8.75;
-        double programming = 8.5;
+
+        byte age = 18;//врядли кому - то больше 255
+
+        //сделал баллы по предметам дробными, т.к. в задании было указано что тип переменной должен быть с плавающей запятой
+        float math = 10F;
+        float physics = 8.75F;
+        float programming = 8.5F;
         Console.WriteLine($"My full name is {fullName}, im {age} years old\n here you can see my marks:" +
             $"\n math - {math}\n physics - {physics}\n programming - {programming}\n you can keep in touch with me using this email - {email}");
+        Console.ReadKey();
+        double sum = math + physics + programming;
+        double average = sum/3;
+        Console.WriteLine("sum of all marks is {0} and there is only 3 subjects, so the avarege is {1:0.00}", sum, average);
     }
 }
